fffe148ee39011f43677d7b0cef24aa444c6ac24
Hanzecode <hiephoang97864@gmail.com>
2025-06-23 12:25:57 +0100

    initial commit

--- project_sql/5. optimise query.sql
+++ project_sql/5. optimise query.sql
@@ -1,3 +1,8 @@
+/* Question: What are the most optimal skills to learn loka it's in high demand and a high-paying skill)?"
+- Identify skills in high demand and assoliated with high average salaries for Data Analyst roles
+- Concentrates on remote positions with specified Malacies
+- Why? Targets skills that offer job security (high demand) and financial benefits (high salaries), offering strategic insights for career development in data analysis */
+
 WITH demanded_skills AS (
 SELECT
     skills_job_dim.skill_id,
